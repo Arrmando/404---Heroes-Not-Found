54fd7aebec9d4e0c7b9015121e36b6aead0fbf2a
root <root@FACHINELLI.localdomain>
2025-01-18 20:20:45 -0300

    feat: add button

---
+++ src/ui/Components/include/Button.hpp
@@ -0,0 +1,21 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+class Button {
+private:
+    sf::RectangleShape shape;
+    sf::Text text;
+
+public:
+    Button(float x, float y, float width, float height, const sf::Color& color, const std::string& label, sf::Font& font);
+    void draw(sf::RenderWindow& window);
+    bool isClicked(const sf::Vector2i& mousePos);
+    bool isHover(const sf::Vector2i& mousePos) const;
+    void setText(const std::string& label); 
+    void setColor(const sf::Color& color);
+};
+
+#endif
