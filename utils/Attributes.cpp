a2f21aff80a4ad99bde14988eaa787add8953779
dev03 <dev03@agsin.com>
2025-01-30 16:49:13 -0300

    feat: implement utility cases following defined signatures

---
+++ utils/Attributes.cpp
@@ -0,0 +1,7 @@
+struct Attributes {
+    float hp; 
+    float magicAttack;
+    float physicalAttack;
+    float magicDefense;
+    float physicalDefense;
+};
---
+++ utils/Race.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "./Attributes.hpp"
+#include "./Species.hpp"
+
+struct Race{
+    Species species;
+    std::string description;
+    Attributes attributes;   // Basic attributes of race
+    Attributes modifiers;    // Modifiers of attributes
+};
