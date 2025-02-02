e7c8705208826c9840b367c829d09e372ff89974
EliasDMS <eliasdaniel_ms@outlook.com.br>
2025-02-02 11:51:57 -0300

    Feat: alterações no playersheetholder feitas por mim

--- src/model/character/PlayerSheetHolder.cpp
+++ src/model/character/PlayerSheetHolder.cpp
@@ -2,9 +2,10 @@
 #include <memory>
 #include "./include/TechnicalSheet.hpp"
 
-
 class PlayerSheetHolder { 
+    
     // Atributos privados da classe
+
     int level = 1;
     std::string name = "Morpheus";
     Species species_ = Human;
@@ -13,41 +14,41 @@ class PlayerSheetHolder {
 
 public:
     // Construtor que inicializa o player com os valores de level, name, species e classes
+
     PlayerSheetHolder() 
         : player(level, name, species_, classes_) {}
 
-
     // Métodos para atualizar os atributos e o player
+
     void defineLevel(int receivedlevel){
         level = receivedlevel;
-        player = TechnicalSheet(level, name, species_, classes_);
+        player.setLevel(level);  // Atualiza o nível diretamente no objeto player
     }
 
-    void defineName(std::string receivedname){
+    void defineName(const std::string& receivedname){
         name = receivedname;
-        player = TechnicalSheet(level, name, species_, classes_);
+        player.setName(name);  // Atualiza o nome diretamente no objeto player
     }
 
     void defineSpecies(Species receivedspecies){
         species_ = receivedspecies;
-        player = TechnicalSheet(level, name, species_, classes_);
+        player.setSpecies(species_);  // Atualiza a espécie diretamente no objeto player
     }
 
     void defineClasses(Classes receivedclasses){
         classes_ = receivedclasses;
-        player = TechnicalSheet(level, name, species_, classes_); 
+        player.setClasses(classes_);  // Atualiza a classe diretamente no objeto player
     }
 
-   
-    //Método gett para player, não permite alterações em player
+    // Método get para player, não permite alterações em player
+
     const TechnicalSheet& getPlayerConst() const {
-    return player;
-    //Não sei se terá aplicação viável o encapsulamento lógico em TechnicalSheet já é forte o baste
+        return player;
     }
 
-    //Método get que permite alterar o objeto player
-    TechnicalSheet& getPlayer() {
-    return player;  
-}
+    // Método get que permite alterar o objeto player
 
-}
+    TechnicalSheet& getPlayer() {
+        return player;  
+    }
+};
